444988dd331701f078fb5057c28898961550f56f
Yoones Khoshghadam <yooneskh@gmail.com>
2022-12-11 12:01:12 +0330

    chore (dependencies): update playground dependencies

--- lib/unified-form.vue
+++ lib/unified-form.vue
@@ -17,7 +17,7 @@ const emit = defineEmits([
 
 /* fields */
 
-import { getRegisteredElements, getRegisteredTransformers } from 'unified-form';
+import { getRegisteredElements, getRegisteredTransformers } from '../mod';
 import { matches } from 'unified-mongo-filter';
 
 const transformedFields = computed(() => {
